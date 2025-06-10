3994ee042389d737cf674b25cdb8d2615986932b
stevencabezas <cabezasstevenr@gmail.com>
2025-06-10 11:35:49 -0600

    adding info whatsapp js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function toggleMenu() {
     const navbar = document.querySelector('header .navbar');
     navbar.classList.toggle('active');
 }
+
+function redirectToWhatsApp(brand, product, price) {
+    const message = `Hola, estoy interesado en el perfume ${product} de ${brand}, que tiene un precio de ${price}. ¿Podrían darme más información?`;
+    const phoneNumber = '+50687683732'; 
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank'); 
+}
